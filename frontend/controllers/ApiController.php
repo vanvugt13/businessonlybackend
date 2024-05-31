0edb6f83c7bdac9950c44c53d2d230eb590191fa
Erik van Vugt <vanvugt@s4financials.com>
2024-05-31 19:55:49 +0200

    Fix

--- frontend/controllers/ApiController.php
+++ frontend/controllers/ApiController.php
@@ -301,7 +301,6 @@ class ApiController extends Controller
     {
         $rawdata = file_get_contents("php://input");
         $post_value = (array)json_decode($rawdata);
-        print_R($post_value);
         if(isset($post_value[0])){
             $email = $post_value[0];
             $user = User::find()->where(['email'=>$email])->one();
