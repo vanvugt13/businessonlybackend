e60c7bb0e210aa6f1bd5d896489e71c360aafafd
Erik van Vugt <vanvugt@s4financials.com>
2024-05-31 20:00:22 +0200

    Fix

--- common/models/LoginForm.php
+++ common/models/LoginForm.php
@@ -32,6 +32,12 @@ class LoginForm extends Model
         ];
     }
 
+    public function attributeLabels()
+    {
+        return [
+            'password'=>'Wachtwoord',
+        ];
+    }
     /**
      * Validates the password.
      * This method serves as the inline validation for password.
