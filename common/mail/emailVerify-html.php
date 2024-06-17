6fed83ce2d3bab6c7d8dee8a5b6e8e49da80b800
Erik van Vugt <vanvugt@s4financials.com>
2024-06-17 22:42:08 +0200

    Fix

--- common/mail/emailVerify-html.php
+++ common/mail/emailVerify-html.php
@@ -10,7 +10,24 @@ $verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['site/verify-email', 'to
 <div class="verify-email">
     <p>Hallo <?= Html::encode($user->username) ?>,</p>
 
-    <p>Klik op de onderstaande link om het mailadres te verifieren:</p>
+    <p>Wat leuk dat je een account hebt aangevraagd voor de business app van VVOG.<br><br>
 
-    <p><?= Html::a(Html::encode($verifyLink), $verifyLink) ?></p>
+Om er zeker van te zijn dat we het juiste email adres van je hebben, klik hieronder op de activatie button en je ontvangt direct daarna een email met instructies en inlog gegevens.
+<br><br>
+ 
+
+<?= Html::a(Html::encode($verifyLink), $verifyLink) ?>
+<br><br>
+
+ 
+
+Deze businessclub app is een product van businessonly.nl. Voor vragen kun je contact opnemen via sales@businessonly.nl of kijk op www.businessonly.nl
+
+<br><br>
+
+Met vriendelijke groeten
+<br><br>
+Team Onboarding</p>
+<p>Businessonly.nl</p>
+
 </div>
