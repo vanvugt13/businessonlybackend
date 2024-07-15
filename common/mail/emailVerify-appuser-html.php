b7b67765e2d206a9c88bdcc60ea206da835ba299
Erik van Vugt <vanvugt@s4financials.com>
2024-07-15 21:31:51 +0200

    Activeer nu

--- common/mail/emailVerify-appuser-html.php
+++ common/mail/emailVerify-appuser-html.php
@@ -6,7 +6,8 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var common\models\User $user */
 
-$verifyLink = User::baseAppLoginUrl().Yii::$app->urlManager->createUrl(['/site/verify-email', 'token' => $user->verification_token]);
+$verifyLink = User::baseAppLoginUrl();
+$verifyLink = Yii::$app->urlManager->createAbsoluteUrl(['/site/verify-email', 'token' => $user->verification_token]);
 ?>
 <div class="verify-email">
     <p>Hallo,</p>
