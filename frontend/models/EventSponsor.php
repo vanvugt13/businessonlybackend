f713e91e76862a146748aa734bb3e70828465c5d
Erik van Vugt <vanvugt@s4financials.com>
2024-08-28 22:04:19 +0200

    Fix

--- frontend/models/EventSponsor.php
+++ frontend/models/EventSponsor.php
@@ -98,7 +98,7 @@ class EventSponsor extends \yii\db\ActiveRecord
         $mailer->to = $mailer->getBeheerderMail();//Yii::$app->params['beheerderMail']??null;
         $mailer->from = 'sales@businessonly.nl';
         $mailer->subject = 'Nieuwe sponsor aangemeld';
-        $mailer->body = 'Sponsor '.$this->user->contactpersoon .'('.$this->user->username.') heeft zich aangemeld voor '.$this->getSponsorType().' sponsoring wedstrijd. Klik op de '.Html::a('link','https://www.vvog.nl/teams/senioren/1/wedstrijd/'.$this->event_id,['target'=>'_new']);
+        $mailer->body = 'Sponsor '.$this->user->contactperson .'('.$this->user->username.') heeft zich aangemeld voor '.$this->getSponsorType().' sponsoring wedstrijd. Klik op de '.Html::a('link','https://www.vvog.nl/teams/senioren/1/wedstrijd/'.$this->event_id,['target'=>'_new']);
         $mailer->send();
     }
 }
