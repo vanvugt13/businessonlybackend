6e5456e9299e2f80706ad024a92c1b252dfd3daa
Erik van Vugt <vanvugt@s4financials.com>
2024-12-02 21:36:50 +0100

    Fix

--- frontend/views/site/_password-changed-appuser.php
+++ frontend/views/site/_password-changed-appuser.php
@@ -1,1 +1,7 @@
+<?php
+
+use yii\helpers\Html;
+?>
 Het wachtwoord wijzigen is geslaagd. U kunt inloggen op de app.
+
+<?=Html::a('Klik hier om naar de app te gaan',Yii::$app->params["appUrl"],['class'=>'btn btn-success']);?>
