406ee9066dd4b36eb6137c114ac1850a53b239eb
Erik van Vugt <vanvugt@s4financials.com>
2025-01-16 21:39:46 +0100

    Fix

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -7,7 +7,7 @@ $this->title = 'Home';
 <div class="site-index">
     <div class="p-5 mb-4 bg-transparent rounded-3">
         <div class="container-fluid py-5 text-center">
-            <h1 class="display-4">Welkom op de businessonly backend VVOG</h1>
+            <h1 class="display-4">Welkom op de businessonly App backend </h1>
         </div>
     </div>
 
