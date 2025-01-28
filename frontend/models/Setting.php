e75fff0d93c31cc938ecbad238de6047e3665402
Erik van Vugt <vanvugt@s4financials.com>
2025-01-28 20:33:17 +0100

    Fix

--- frontend/models/Setting.php
+++ frontend/models/Setting.php
@@ -120,7 +120,7 @@ class Setting extends \yii\db\ActiveRecord
         $imagesToCreate=[72,96,128,144,152,192,384,512];
         foreach($imagesToCreate as $neededSize){
             $tempPathResized = Yii::getAlias('@webroot').'/images/icon-'.$neededSize.'x'.$neededSize.'.png';
-            if(file_exists($tempPathResized)) continue;
+            //if(file_exists($tempPathResized)) continue;
             file_put_contents($tempPath,$this->favo_icon);
             $source = imagecreatefrompng($tempPath);
             list($width, $height) = getimagesize($tempPath);
